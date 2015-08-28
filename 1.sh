5bd5d15870a65fc06c0c8628702627e500f5aa27
xing <79837383@qq.com>
2015-08-27 20:30:44 -0700

    log

--- 1.sh
+++ 1.sh
@@ -1,2 +1,18 @@
-#!/bin/sh
+#!/usr/bin/python
 
+a=4
+b=5
+def fun(x,y):
+    res=x+y
+    return res
+
+print "a = %d , b = %d"%(a,b)
+if a==b:
+    print "a=="
+elif a<b:
+    print "a<b"
+    print "a<b"
+else:
+    print "a>b"
+
+print(fun(5,6))
